f079a81c9acea708d2e5775abfa4ba9dbf3ea6d5
Eric Hicks <ehicks05@yahoo.com>
2025-04-03 00:46:24 -0400

    types

--- be/src/services/tmdb/types/base.ts
+++ be/src/services/tmdb/types/base.ts
@@ -66,17 +66,17 @@ export interface Person {
 	adult: boolean;
 	also_known_as: string[];
 	biography: string;
-	birthday: string;
-	deathday?: string;
+	birthday: string | null;
+	deathday: string | null;
 	gender: 0 | 1 | 2 | 3;
-	homepage?: string;
+	homepage: string | null;
 	id: number;
-	imdb_id: string;
+	imdb_id: string | null;
 	known_for_department: string;
 	name: string;
-	place_of_birth?: string;
+	place_of_birth: string | null;
 	popularity: number;
-	profile_path?: string;
+	profile_path: string | null;
 }
 
 export type ProductionCompany = Pick<
