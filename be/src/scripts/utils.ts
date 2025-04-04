a0941ba44ba3645fb2c963659cd2826dbf9ee367
Eric Hicks <ehicks05@yahoo.com>
2025-04-04 16:45:42 -0400

    folder

--- be/src/scripts/utils.ts
+++ be/src/scripts/utils.ts
@@ -1,6 +1,9 @@
 import type { FileType } from './types.js';
 
-export const getPath = (type: FileType) => `./scripts-data/${type}.txt`;
+const prod = process.env.NODE_ENV === 'production';
+const base = prod ? '/app/storage/' : './scripts-data/';
+
+export const getPath = (type: FileType) => `${base}${type}.txt`;
 
 export const consoleLogInPlace = (input: string) => {
 	process.stdout.clearLine(0);
