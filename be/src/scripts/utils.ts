c784e8145d4dd6de1573e487eae9a80e9ff3cedc
Eric Hicks <ehicks05@yahoo.com>
2025-04-05 16:01:29 -0400

    hmm

--- be/src/scripts/utils.ts
+++ be/src/scripts/utils.ts
@@ -8,6 +8,9 @@ logger.info(`writing logs to ${base}`);
 export const getPath = (type: FileType) => `${base}${type}.txt`;
 
 export const consoleLogInPlace = (input: string) => {
+	if (prod) {
+		return;
+	}
 	process.stdout.clearLine(0);
 	process.stdout.cursorTo(0);
 	process.stdout.write(input);
