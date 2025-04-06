5e132c98055bfe4dc0c5420e0ea45f4a2df3b7a0
Eric Hicks <ehicks05@yahoo.com>
2025-04-06 14:55:26 -0400

    tweak

--- be/src/scripts/runSyncJob.ts
+++ be/src/scripts/runSyncJob.ts
@@ -3,24 +3,22 @@ import logger from '~/services/logger.js';
 import prisma from '~/services/prisma.js';
 import { runSync } from './sync.js';
 
+const taskName = 'db sync task';
+
 export const runSyncJob = async () => {
-	const { id: logId } = await prisma.syncRunLog.create({ data: {} });
+	logger.info(`starting ${taskName}`);
+
+	const { id } = await prisma.syncRunLog.create({ data: {} });
 
 	try {
-		logger.info('starting tmdb_loader script');
-
 		await runSync();
 	} catch (err) {
 		logger.error(err);
-	} finally {
-		const log = await prisma.syncRunLog.update({
-			data: { endedAt: new Date() },
-			where: { id: logId },
-		});
-		const duration = intervalToDuration({
-			start: log.createdAt,
-			end: log.endedAt || 0,
-		});
-		logger.info(`finished tmdb_loader script in ${formatDuration(duration)}`);
 	}
+
+	const endedAt = new Date();
+	const log = await prisma.syncRunLog.update({ data: { endedAt }, where: { id } });
+	const duration = intervalToDuration({ start: log.createdAt, end: endedAt });
+
+	logger.info(`finished ${taskName} in ${formatDuration(duration)}`);
 };
