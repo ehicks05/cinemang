61eaea0014980bc69986618ab883026bf3485da3
Eric Hicks <ehicks05@yahoo.com>
2025-04-05 21:25:27 -0400

    hm

--- be/src/scripts/vacuumAnalyze.ts
+++ be/src/scripts/vacuumAnalyze.ts
@@ -1,8 +1,8 @@
 import logger from '~/services/logger.js';
-import prisma from '~/services/prisma.js';
+import { prismaDirect } from '~/services/prisma.js';
 
 export const vacuumAnalyze = async () => {
 	logger.info('running vacuum analyze');
-	await prisma.$executeRaw`vacuum analyze`;
+	await prismaDirect.$executeRaw`vacuum analyze`;
 	logger.info('finished running vacuum analyze');
 };
--- be/src/services/prisma.ts
+++ be/src/services/prisma.ts
@@ -1,7 +1,11 @@
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
+const prismaDirect = new PrismaClient({
+	datasourceUrl: process.env.DIRECT_DATABASE_URL,
+});
 
 export default prisma;
+export { prismaDirect };
 
 export const PRISMA_CONCURRENCY = { concurrency: 8 };
