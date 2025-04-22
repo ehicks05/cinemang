7945768f6e3d3ebda6a5b020be71e513f2d1bb0b
Eric Hicks <ehicks05@yahoo.com>
2025-04-22 17:41:50 -0400

    update

--- be/src/scripts/fetchAndSave/discoverMediaIds.ts
+++ be/src/scripts/fetchAndSave/discoverMediaIds.ts
@@ -8,8 +8,16 @@ type Media = 'movie' | 'tv';
 export const discoverMediaIds = async (media: Media, isFullMode: boolean) => {
 	logger.info('discovering media ids');
 
-	const start = isFullMode ? undefined : subMonths(new Date(), 3);
-	const results = await tmdb.discover({ media, start, minVotes: MIN_VOTES });
+	const start = isFullMode ? undefined : subMonths(new Date(), 3).toISOString();
+
+	const results = await tmdb.discover({
+		media,
+		query: {
+			'vote_count.gte': MIN_VOTES,
+			'first_air_date.gte': media === 'tv' ? start : undefined,
+			'release_date.gte': media === 'movie' ? start : undefined,
+		},
+	});
 	const mediaIds = results.map((o) => o.id);
 
 	logger.info('done discovering media ids');
