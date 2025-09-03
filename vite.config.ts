1f96c5338da35afd63c523da0540caaf8f8d089b
Eric Hicks <ehicks05@yahoo.com>
2025-09-03 12:00:05 -0400

    plugin-react

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import tsConfigPaths from 'vite-tsconfig-paths'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [
+    react(),
     tsConfigPaths({
       projects: ['./tsconfig.json'],
     }),
